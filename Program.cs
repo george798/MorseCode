55e776df1332ea2a676e77104d7a10662351f97e
george798 <giorgoskal721@gmail.com>
2017-08-07 19:25:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseCode
+{
+    public class Program
+ {
+    private static Dictionary<char, string> _morseAlphabetDictionary;
+
+    static void Main()
+    {
+        InitializeDictionary();
+
+        Console.WriteLine("Input");
+        string userInput = UserInput();
+        Console.WriteLine("Morse output: " + MorseEncode(userInput));
+
+        Console.WriteLine("[Press ANY KEY to exit]");
+        Console.ReadLine();
+    }
+
+    public static void InitializeDictionary()
+    {
+        _morseAlphabetDictionary = new Dictionary<char, string>()
+                                   {
+                                       {'a', ".-"},
+                                       {'b', "-..."},
+                                       {'c', "-.-."},
+                                       {'d', "-.."},
+                                       {'e', "."},
+                                       {'f', "..-."},
+                                       {'g', "--."},
+                                       {'h', "...."},
+                                       {'i', ".."},
+                                       {'j', ".---"},
+                                       {'k', "-.-"},
+                                       {'l', ".-.."},
+                                       {'m', "--"},
+                                       {'n', "-."},
+                                       {'o', "---"},
+                                       {'p', ".--."},
+                                       {'q', "--.-"},
+                                       {'r', ".-."},
+                                       {'s', "..."},
+                                       {'t', "-"},
+                                       {'u', "..-"},
+                                       {'v', "...-"},
+                                       {'w', ".--"},
+                                       {'x', "-..-"},
+                                       {'y', "-.--"},
+                                       {'z', "--.."},
+                                       {'0', "-----"},
+                                       {'1', ".----"},
+                                       {'2', "..---"},
+                                       {'3', "...--"},
+                                       {'4', "....-"},
+                                       {'5', "....."},
+                                       {'6', "-...."},
+                                       {'7', "--..."},
+                                       {'8', "---.."},
+                                       {'9', "----."}
+                                   };
+    }
+
+    public static string UserInput()
+    {
+        string input = Console.ReadLine();
+
+        if ( string.IsNullOrEmpty(input))
+        {
+            input = input.ToLower();
+        }
+
+        return input;
+    }
+
+    public static string MorseEncode(string input)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (char character in input)
+        {
+            if (_morseAlphabetDictionary.ContainsKey(character))
+            {
+                stringBuilder.Append(_morseAlphabetDictionary[character] + " ");
+            }
+            else if (character == ' ')
+            {
+                stringBuilder.Append("/ ");
+            }
+            else
+            {
+                stringBuilder.Append(character + " ");
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+ 
+  }
+}
